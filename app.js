7757a7121e43bbf155e20885eeb00f18b0600214
Rosangela Rocha <ro.rochatur@gmail.com>
2022-03-20 16:31:27 -0300

    aprendendo roteamento e rotas parametrizadas

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const rotasProdutos = require('./rotas/rotasProdutos');
 let app = express();
 
 app.get("/", (req, res) => {
     res.send("Olá mundo vivo!");
 });
+app.get('/contato', (req, res)=>res.send("Pagina de contatos"));
+//app.get('/produtos/:id?', (req, res)=>{
+//    let {id} = req.params;
+//    res.send("Eu tenho um produto com o id: " + id)
+//});
+
+app.use('/produtos', rotasProdutos);
+
 app.listen(3000, () => 
 console.log("Servidor executando"));
---
+++ rotas/rotasProdutos.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/criar', (req, res)=>res.send("Criando um produto"));
+router.get('/deletar', (req, res)=>res.send("Deletando um produto"));
+
+module.exports = router;
